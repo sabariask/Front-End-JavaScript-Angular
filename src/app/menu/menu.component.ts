bd862687b589c175383583852d195bfd0cb562a1
sabariask <sabarinathan.mani13@gmail.com>
2022-10-17 10:27:56 +0530

    Services added

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Dish} from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,7 +9,7 @@ import { DISHES } from '../shared/dishes';
 })
 export class MenuComponent implements OnInit {
 
-  dishes:Dish[]= DISHES;
+  dishes:Dish[] = [];
 
   selectedDish:Dish|undefined;
 
@@ -17,10 +17,10 @@ export class MenuComponent implements OnInit {
     this.selectedDish = dish;
   }
 
-  constructor() { }
+  constructor(private dishService:DishService) { }
 
   ngOnInit(): void {
-
+    this.dishes = this.dishService.getDishes();
   }
 
 }
